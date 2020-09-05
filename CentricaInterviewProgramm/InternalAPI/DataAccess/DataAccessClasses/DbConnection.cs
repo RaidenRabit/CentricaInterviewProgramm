7dac2dd2af6b678da6bc9e3aee4652c9f668e28c
Andrei-Eugen Birta <1062021@ucn.dk>
2020-09-05 18:24:50 +0200

    new connection string

--- CentricaInterviewProgramm/InternalAPI/DataAccess/DataAccessClasses/DbConnection.cs
+++ CentricaInterviewProgramm/InternalAPI/DataAccess/DataAccessClasses/DbConnection.cs
@@ -7,7 +7,7 @@ namespace InternalAPI.DataAccess.DataAccessClasses
     {
         private static DbConnection _instance;
         private SqlConnection _con = null;
-        private string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CentricaProgram;Integrated Security=SSPI";
+        private string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CentricaProgram;Integrated Security=FALSE; User ID=sa; password=Password12!";
 
         public static DbConnection GetInstance()
         {
