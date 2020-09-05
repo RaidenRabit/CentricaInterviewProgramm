3c486472d8f86f46ff89d2d684c4939c3db5de77
Andrei-Eugen Birta <1062021@ucn.dk>
2020-09-05 17:49:18 +0200

    moving tests connection to DbConnection

--- CentricaInterviewProgramm/APITests/DatabaseTesting.cs
+++ CentricaInterviewProgramm/APITests/DatabaseTesting.cs
@@ -3,6 +3,7 @@ using Microsoft.Data.SqlClient;
 using System.IO;
 using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
+using InternalAPI.DataAccess.DataAccessClasses;
 
 namespace APITests
 {
@@ -10,13 +11,13 @@ namespace APITests
     {
         public static void ResetDatabase()
         {
-            string connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CentricaProgram;Integrated Security=SSPI";
             string insertScriptPath = "../../.. /../../../Others/InsertDataScript.sql";
             string createScriptPath = "../../../../Others/CreateTablesScript.sql";
             string insertScript = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, insertScriptPath));
             string createScript = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, createScriptPath));
 
-            using (SqlConnection conn = new SqlConnection(connection))
+            DbConnection con = DbConnection.GetInstance();
+            using (SqlConnection conn = con.GetConnection())
             {
                 Server db = new Server(new ServerConnection(conn));
                 db.ConnectionContext.ExecuteNonQuery(createScript);
