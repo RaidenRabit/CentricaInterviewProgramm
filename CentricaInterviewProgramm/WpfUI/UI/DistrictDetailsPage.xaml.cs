7f02a927f7e926434976e4a29f2b5e5b12045a46
Andrei-Eugen Birta <1062021@ucn.dk>
2020-09-07 20:19:42 +0200

    added district details page

---
+++ CentricaInterviewProgramm/WpfUI/UI/DistrictDetailsPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using InternalAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfUI.UI
+{
+    /// <summary>
+    /// Interaction logic for DistrictDetails.xaml
+    /// </summary>
+    public partial class DistrictDetailsPage : Page
+    {
+        private District _district;
+        public DistrictDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        public void SetDistrict(District district)
+        {
+            _district = district;
+            PopulatePage();
+        }
+
+        private void PopulatePage()
+        {
+            if (_district == null)
+            {
+                return;
+            }
+
+            districtName_lbl.Content = _district.DistrictName;
+            salesPerson_lbl.Content = $"SalesPerson Count: {_district.Spwr.Count}";
+            store_lbl.Content = $"Stores Count: {_district.Stores.Count}";
+
+            PopulateGrids(_district.Stores, store_grid);
+            PopulateGrids(_district.Spwr, salesPerson_grid);
+        }
+
+        private void PopulateGrids<T>(List<T> objects, Grid grid)
+        {
+            int initialColumn = 0;
+            int row = grid.RowDefinitions.Count;
+            int column = 0;
+
+            foreach (var obj in objects)
+            {
+                foreach (var property in obj.GetType().GetProperties())
+                {
+                    string content = "";
+                    var objValue = property.GetValue(obj, null);
+                    var type = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+                    if (type == typeof (SalesPerson))
+                    {
+                        foreach (var objValueProperty in objValue.GetType().GetProperties())
+                        {
+                            content += objValueProperty.GetValue(objValue, null).ToString() + "\n";
+                        }
+                    }
+                    else
+                    {
+                        content = objValue.ToString();
+                    }
+                    var label = new Label();
+                    label.Content = content;
+                    Grid.SetColumn(label, column);
+                    Grid.SetRow(label, row);
+                    grid.Children.Add(label);
+                    column++;
+                }
+                column = initialColumn;
+                grid.RowDefinitions.Add(new RowDefinition());
+                row++;
+            }
+        }
+
+
+        private void backBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // instantiate mainwindow and store it in the window variable
+            var window = (MainWindow)Application.Current.MainWindow;
+            var viewAllDistrictsPage = new ViewAllDistrictsPage();
+            window.Content = viewAllDistrictsPage;
+        }
+    }
+}
--- CentricaInterviewProgramm/WpfUI/UI/MainWindow.xaml.cs
+++ CentricaInterviewProgramm/WpfUI/UI/MainWindow.xaml.cs
@@ -1,11 +1,5 @@
-﻿using InternalAPI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using WpfUI.Controllers;
+﻿using System.Windows;
+using WpfUI.UI;
 
 namespace WpfUI
 {
@@ -14,73 +8,14 @@ namespace WpfUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DistrictService _districtService;
-        private string _originalDistrictCountText;
 
         public MainWindow()
         {
             InitializeComponent();
-            _districtService = new DistrictService();
+            var viewAllDistrictsPage = new ViewAllDistrictsPage();
+            this.Content = viewAllDistrictsPage;
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (String.IsNullOrWhiteSpace(_originalDistrictCountText))
-            {
-                _originalDistrictCountText = districtCount_lbl.Text;
-            }
-            var districtTotalCount =  await _districtService.GetDistrictCount();
-            districtCount_lbl.Text = _originalDistrictCountText + districtTotalCount.ToString();
-
-            var districts = await _districtService.GetAllDistricts();
-            if (districts != null)
-            {
-                CreateDistrictButtons(districts);
-            }
-        }
-
-        private void SpecificDistrictBtn_Clicked(object sender, EventArgs e)
-        {
-            var button = (Button) sender;
-            districtCount_lbl.Text = button.Content + " got clicked";
-            this.NavigationService.Navigate(new Uri("Pages/Page2.xaml", UriKind.Relative));
-        }
-
-        private void CreateDistrictButtons(List<District> districts)
-        {
-            int row = gridMain.ColumnDefinitions.Count;
-            int column = gridMain.RowDefinitions.Count;
-
-            gridMain.ColumnDefinitions.Add(new ColumnDefinition());
-            gridMain.ColumnDefinitions.Add(new ColumnDefinition());
-            gridMain.ColumnDefinitions.Add(new ColumnDefinition());
-            gridMain.ColumnDefinitions.Add(new ColumnDefinition());
-
-            foreach (var district in districts)
-            {
-                Button button = new Button();
-                button.Click += (s, e) => SpecificDistrictBtn_Clicked(s, e);
-                string btnContent = $"{district.DistrictName} \nSalesPersons: {district.Spwr.Count} \nStores:{district.Stores.Count}";
-                button.Content = district.DistrictName;
-                button.Background = Brushes.LightGray;
-                if (!district.Spwr.Any())
-                {
-                    button.Background = Brushes.Gray;
-                }
-                Grid.SetColumn(button, column);
-                Grid.SetRow(button, row);
-                gridMain.Children.Add(button);
-                
-
-                column++;
-                if (column == 5)
-                {
-                    column = 1;
-                    gridMain.RowDefinitions.Add(new RowDefinition());
-                    row++;
-                }
-            }
-        }
     }
         
 }
